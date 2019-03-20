4da3ebfe6c682fd01a6fc9e96e1f96656cfb0770
Patrick Bucher <patrick.bucher@peax.ch>
2019-03-20 07:58:14 +0100

    do not use echo -n but service list without trailing newline

--- compose.sh
+++ compose.sh
@@ -2,10 +2,10 @@
 
 source ./services.list
 
-echo -n "$SERVICES" | while read -r LINE; do
-    NAME=$(echo $LINE | cut -d':' -f1)
-    PORT=$(echo $LINE | cut -d':' -f2)
+echo "$SERVICES" | while read -r LINE; do
+    NAME="$(echo $LINE | cut -d':' -f1)"
+    PORT="$(echo $LINE | cut -d':' -f2)"
     docker build . --build-arg PORT=$PORT --build-arg FILE_BASE_NAME=$NAME -t $NAME
-    docker rm -f $NAME
+    docker rm -f $NAME 2>/dev/null
     docker run -dit -p $PORT:$PORT --name $NAME $NAME
 done
--- management-dashboard.sh
+++ management-dashboard.sh
@@ -2,8 +2,8 @@
 
 source ./services.list
 
-echo -n "$SERVICES" | while read -r LINE; do
-    NAME=$(echo $LINE | cut -d':' -f1)
-    PORT=$(echo $LINE | cut -d':' -f2)
+echo "$SERVICES" | while read -r LINE; do
+    NAME="$(echo $LINE | cut -d':' -f1)"
+    PORT="$(echo $LINE | cut -d':' -f2)"
     curl localhost:$PORT/$NAME
 done
