ce495bddf12858413e468ad4e10cf5d5dc2043f2
patrickbucher <patrick.bucher@stud.hslu.ch>
2019-04-07 14:52:31 +0200

    seed random number generator in order to enable better management decisions

--- dice.go
+++ dice.go
@@ -5,9 +5,11 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
+	"time"
 )
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
 	http.HandleFunc("/dice", func(w http.ResponseWriter, r *http.Request) {
 		eyes := rand.Intn(6) + 1
 		w.Write([]byte(fmt.Sprintf("Roll the Dice: %d\n", eyes)))
